b06d5ec193a88679ad2bb486ad285ecd79321547
zengchao <sccc1756461653@163.com>
2023-03-12 22:26:01 +0800

    First commit by myself 3/12

---
+++ src/main/java/com/wut/self/UserCenterBackendSelfApplication.java
@@ -0,0 +1,15 @@
+package com.wut.self;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.wut.self.mapper")
+public class UserCenterBackendSelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterBackendSelfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wut/self/UserCenterBackendSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wut.self;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCenterBackendSelfApplicationTests {
+
+    @Test
+    void testSQL() {
+
+    }
+}
