c039f9c92b2c12752bde4020e3a4a74031cdeeb5
zengchao <sccc1756461653@163.com>
2023-04-24 11:43:42 +0800

    date: 4/24 commit by zeng; update: sql

--- doc/sql/user-center.sql
+++ doc/sql/user-center.sql
@@ -1,7 +1,7 @@
 create table user
 (
     id            bigint auto_increment comment 'id'
-        primary key,
+    primary key,
     username      varchar(256)                       null comment '用户名',
     user_account  varchar(256)                       null comment '登录账号',
     avatar_url    varchar(1024)                      null comment '头像',
@@ -14,7 +14,5 @@ create table user
     update_time   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
     is_deleted    tinyint  default 0                 not null comment '是否删除',
     user_role     int      default 0                 not null comment '0-普通用户; 1-管理员; 2-vip用户',
-    validate_code varchar(256)                       null comment '用户校验码',
-    tags          varchar(1024)                      null comment '标签列表 json',
-    user_profile  varchar(512)                       null comment '个人简介'
+    validate_code varchar(256)                       null comment '用户校验码'
 ) character set utf8 engine innodb;
